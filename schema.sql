6677a74be90ef0b30377cff6c24b06a2b8d20487
TobynSitar <89927895+TobynSitar@users.noreply.github.com>
2024-04-28 21:26:13 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user
 (
-   id            INTEGER PRIMARY KEY AUTOINCREMENT,
+    id            INTEGER PRIMARY KEY AUTOINCREMENT,
     username      TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     created_at    DATETIME DEFAULT CURRENT_TIMESTAMP

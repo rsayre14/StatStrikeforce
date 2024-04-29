e2f844763a649f7dfe1c10c74ba1686dd6f9612c
TobynSitar <89927895+TobynSitar@users.noreply.github.com>
2024-04-28 21:24:40 -0400

    Update schema.sql
    modified fields for user username (email) , and hashed password

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE user
 (
-    id         INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id    TEXT NOT NULL,
-    prediction INTEGER CHECK (prediction BETWEEN 1 AND 10 OR prediction IS NULL)
+   id            INTEGER PRIMARY KEY AUTOINCREMENT,
+    username      TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at    DATETIME DEFAULT CURRENT_TIMESTAMP
 );

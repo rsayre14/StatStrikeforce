bac432a5ea55fe254e3db23fb8c5ad1a6c5fd92f
Ryan Sayre <RS998689@wcupa.edu>
2024-04-29 22:16:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error
 import numpy as np
 
 app = Flask(__name__)
@@ -20,17 +20,41 @@ def prep_data(attacker, defender):
     attack_features_df = pd.DataFrame([attacker_features])
     defender_features_df = pd.DataFrame([defender_features])
 
+    expanded_df = pd.DataFrame()
+
+    for column in ['KD', 'HS', 'KPR']:
+        temp_df = pd.DataFrame.from_dict(attack_features_df[column][0], orient='index', columns=[column])
+        temp_df['Name'] = attack_features_df['Name'][0]
+        temp_df.reset_index(inplace=True)
+        temp_df.rename(columns={'index': 'Measurement'}, inplace=True)
+        if expanded_df.empty:
+            expanded_df = temp_df
+        else:
+            expanded_df = pd.merge(expanded_df, temp_df, on=['Name', 'Measurement'], how='outer')
+
+    expanded_df1 = pd.DataFrame()
+
+    for column in ['KD', 'HS', 'KPR']:
+        temp_df = pd.DataFrame.from_dict(defender_features_df[column][0], orient='index', columns=[column])
+        temp_df['Name'] = defender_features_df['Name'][0]
+        temp_df.reset_index(inplace=True)
+        temp_df.rename(columns={'index': 'Measurement'}, inplace=True)
+        if expanded_df1.empty:
+            expanded_df1 = temp_df
+        else:
+            expanded_df1 = pd.merge(expanded_df1, temp_df, on=['Name', 'Measurement'], how='outer')
+
     # Prepare target values
-    y_attack = np.array([attacker['WL']])
-    y_defend = np.array([defender['WL']])
+    y_a = np.array([attacker['WL']])
+    y_d = np.array([defender['WL']])
 
-    X_attack = attack_features_df.to_numpy()
-    X_defend = defender_features_df.to_numpy()
+    dictionary = y_a[0]
+    dictionary1 = y_d[0]
 
-    print("Attack Features:\n", attack_features_df)
-    print("Defend Features:\n", defender_features_df)
-    print("Attack WL:", y_attack)
-    print("Defend WL:", y_defend)
+    y_attack = pd.DataFrame(list(dictionary.items()), columns=['Measurement', 'Value'])['Value'].to_numpy()
+    y_defend = pd.DataFrame(list(dictionary1.items()), columns=['Measurement', 'Value'])['Value'].to_numpy()
+    X_attack = expanded_df.drop('Name', axis=1).to_numpy()
+    X_defend = expanded_df1.drop('Name', axis=1).to_numpy()
 
     X_attack_train, X_attack_test, y_attack_train, y_attack_test = train_test_split(X_attack, y_attack, test_size=0.2,
                                                                                     random_state=42)
@@ -46,33 +70,23 @@ def prep_data(attacker, defender):
     # Make predictions
     y_attack_pred = model_attack.predict(X_attack_test)
     y_defend_pred = model_defend.predict(X_defend_test)
-    print("Attacker Model R^2 Score:", r2_score(y_attack, y_attack_pred))
-    print("Defender Model R^2 Score:", r2_score(y_defend, y_defend_pred))
+
+    mse_attack = mean_squared_error(y_attack_test, y_attack_pred)
+    mse_defend = mean_squared_error(y_defend_test, y_defend_pred)
+
+    return mse_attack, mse_defend
 
 
 @app.route('/accept_data', methods=['POST', 'GET'])
 def accept_data():
-    attacker_mock = {
-        'WL': 0.75,
-        'KD': 1.3,
-        'HS': 0.7,
-        'KPR': 0.3
-    }
-    defender_mock = {
-        'WL': 0.65,
-        'KD': 1.1,
-        'HS': 0.6,
-        'KPR': 0.25
-    }
     if request.is_json:
         data = request.get_json()
-        print("Received data:")
         attacker_data = data.get('Attacker', {})
         defender_data = data.get('Defender', {})
-        prep_data(attacker_data, defender_data)
-        return jsonify({"message": "Data received successfully"}), 200
+        mse_a, mse_d = prep_data(attacker_data, defender_data)
+        return jsonify({"mseAttack": mse_a,
+                        "mseDefend": mse_d}), 200
     else:
-        prep_data(attacker_mock, defender_mock)
         return jsonify({"error": "Request must be JSON"}), 400
 
 

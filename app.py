bb500a483a58fdfca7a666f37a816eb80fd72969
Ryan Sayre <RS998689@wcupa.edu>
2024-05-01 12:56:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import pandas as pd
+from flask_cors import CORS
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/', methods=['GET'])

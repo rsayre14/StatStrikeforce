23a43eb675438341e4534744041adc8b8d0bc6f3
Ryan Sayre <RS998689@wcupa.edu>
2024-05-01 12:53:38 -0400

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+echo "Starting Database Initialization..."
 
 # Database initialization logic
-DB_PATH="./your_database.db"
-SCHEMA_PATH="./schema.sql"
+DB_PATH="/app/your_database.db"  # Ensure path is correct
+SCHEMA_PATH="/app/schema.sql"    # Ensure path is correct
 
+echo "Checking if database exists..."
 if [ ! -f "$DB_PATH" ]; then
-    echo "Creating database schema..."
+    echo "Database not found, creating database schema..."
     sqlite3 $DB_PATH < $SCHEMA_PATH
+else
+    echo "Database already exists."
 fi
 
+echo "Database setup complete."
+
 # Start Gunicorn with Flask application
+echo "Starting Gunicorn..."
 exec gunicorn -b :3000 -w 4 app:app

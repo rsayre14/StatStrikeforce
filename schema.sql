f24fba30b5ccb892467120687d23342fc09e10ef
Ryan Sayre <RS998689@wcupa.edu>
2024-05-01 12:54:43 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
-DROP TABLE IF EXISTS user_stats;
-DROP TABLE IF EXISTS user;
+CREATE TABLE user
+(
+    id            INTEGER PRIMARY KEY AUTOINCREMENT,
+    username      TEXT UNIQUE NOT NULL,
+    password_hash TEXT        NOT NULL,
+    r6_user_id    TEXT,
+    created_at    DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE user_stats
 (
@@ -8,13 +14,4 @@ CREATE TABLE user_stats
     mse_attack REAL,
     mse_defend REAL,
     FOREIGN KEY (user_id) REFERENCES user (id)
-);
-
-CREATE TABLE user
-(
-    id            INTEGER PRIMARY KEY AUTOINCREMENT,
-    username      TEXT UNIQUE NOT NULL,
-    password_hash TEXT        NOT NULL,
-    r6_user_id    TEXT,
-    created_at    DATETIME DEFAULT CURRENT_TIMESTAMP
 );
